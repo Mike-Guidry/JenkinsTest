f55a26b8b502d8ef24541ab2c81927476b4e862b
Mike Guidry <michaelguidry@hotmail.com>
2021-11-04 14:02:48 -0500

    Added test C# project.

---
+++ JenkinsApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Specialized;
+
+namespace JenkinsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string sAttr = ConfigurationManager.AppSettings.Get("Env");
+            Console.WriteLine($"Env: {sAttr}");
+
+            NameValueCollection sAll;
+            sAll = ConfigurationManager.AppSettings;
+
+            foreach (string s in sAll.AllKeys)
+                Console.WriteLine("Key: " + s + " Value: " + sAll.Get(s));
+            Console.ReadLine();
+
+        }
+    }
+}
